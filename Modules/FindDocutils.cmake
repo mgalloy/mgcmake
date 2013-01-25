70f17baaea370b9c4edb77e1ace799ebdd65d609
Michael Galloy <mgalloy@txcorp.com>
2013-01-24 17:03:20 -0700

    Adding simple module to find Docutils executables.

---
+++ Modules/FindDocutils.cmake
@@ -0,0 +1,10 @@
+include(FindPackageHandleStandardArgs)
+
+find_program(RST2HTML_EXECUTABLE NAMES rst2html rst2html.py)
+find_package_handle_standard_args(Docutils DEFAULT_MSG RST2HTML_EXECUTABLE)
+
+find_program(RST2LATEX_EXECUTABLE NAMES rst2latex rst2latex.py)
+find_package_handle_standard_args(Docutils DEFAULT_MSG RST2LATEX_EXECUTABLE)
+
+find_program(RST2MAN_EXECUTABLE NAMES rst2man rst2man.py)
+find_package_handle_standard_args(Docutils DEFAULT_MSG RST2MAN_EXECUTABLE)
