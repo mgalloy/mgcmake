9f2444234951726052f09e53caa5625117dc8030
Michael Galloy <mgalloy@gmail.com>
2013-08-26 16:25:01 -0600

    Adding [module]_FOUND variables, some output, normal quiet/required stuff.

--- Modules/FindIDLdoc.cmake
+++ Modules/FindIDLdoc.cmake
@@ -4,3 +4,17 @@ find_path(IDLdoc_DIR
   idldoc.pro
   PATHS "~/software;~/projects"
 )
+
+if (IDLdoc_DIR)
+  set(IDLdoc_FOUND TRUE)
+endif()
+
+if (IDLdoc_FOUND)
+  if (NOT IDLdoc_FIND_QUIETLY)
+    message(STATUS "IDLdoc = ${IDLdoc_DIR}")
+  endif ()
+else ()
+  if (IDLdoc_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find IDLdoc!")
+  endif ()
+endif ()
--- Modules/Findmgunit.cmake
+++ Modules/Findmgunit.cmake
@@ -4,3 +4,17 @@ find_path(mgunit_DIR
   mgunit.pro
   PATHS "~/software;~/projects"
 )
+
+if (mgunit_DIR)
+  set(mgunit_FOUND TRUE)
+endif()
+
+if (mgunit_FOUND)
+  if (NOT mgunit_FIND_QUIETLY)
+    message(STATUS "mgunit = ${mgunit_DIR}")
+  endif ()
+else ()
+  if (mgunit_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find mgunit!")
+  endif ()
+endif ()
