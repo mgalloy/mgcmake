ff2a3e279d23cfe3ae31810f4b9d8b3f6d3872e5
Michael Galloy <mgalloy@gmail.com>
2014-10-01 17:42:51 -0600

    Adding idlwave module.

---
+++ Modules/Findidlwave.cmake
@@ -0,0 +1,20 @@
+include(FindPackageHandleStandardArgs)
+
+find_path(idlwave_DIR
+  idlwave.el
+  PATHS "~/software;~/projects"
+)
+
+if (idlwave_DIR)
+  set(idlwave_FOUND TRUE)
+endif()
+
+if (idlwave_FOUND)
+  if (NOT idlwave_FIND_QUIETLY)
+    message(STATUS "idlwave = ${idlwave_DIR}")
+  endif ()
+else ()
+  if (idlwave_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find idlwave!")
+  endif ()
+endif ()
